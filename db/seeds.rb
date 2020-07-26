359d91324ad496ed03b19c85c718faaae2355b13
redazures <jwu_nyc@yahoo.com>
2020-07-26 11:19:49 -0400

    a few updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-# e=Dungeon.create(name:"entrance",pressure:0,des:"This is the entrance of the dungeon",location:"south",wet:true)
+#e=Dungeon.create(name:"Globin Cave",pressure:0,des:"This is the entrance of the dungeon",location:"south",wet:true)
---
+++ lib/talk.rb
@@ -0,0 +1,14 @@
+module Talk
+    def attack_speak
+      "I am going to attack to you!"
+    end
+   
+    def dying_speak
+      "I am dying. You have slayed me"
+    end
+   
+    def dead_body
+      "stop attacking this dead body"
+    end
+   
+  end
